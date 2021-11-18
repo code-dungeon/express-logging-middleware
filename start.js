82564ac9230fdcd130e73c7a716e4d886b5f65f5
Joe Allen <joe.allen@subsplash.com>
2021-11-17 22:05:20 -0700

    chore(build): updated context-continuation dependency

--- start.js
+++ start.js
@@ -2,8 +2,29 @@ const express = require('express');
 const { ctx } = require('@code-dungeon/context-continuation');
 const { Logger } = require('@code-dungeon/toothpick');
 const { create } = require('./dist');
+const bodyParser = require('body-parser');
+const compress = require('compression');
+const cors = require('cors');
+const hooks = require('async_hooks');
 
 const app = express();
+
+app.use(bodyParser.json({
+  limit: '10mb'
+}));
+
+app.use(bodyParser.urlencoded({
+  extended: false,
+  limit: '15mb'
+}));
+
+app.use(compress());
+
+app.use(cors({
+  credentials: true,
+  origin: true
+}));
+
 const port = 8080;
 const defaultFormats = [
   Logger.Format.context(() => {
@@ -37,11 +58,25 @@ const middlewareLogger = Logger.create({
 
 app.use(create(middlewareLogger));
 // app.use(LoggingMiddleware(entryLogger, exitLogger));
+
+function dostuff(cb) {
+
+  console.log('before nextTick', hooks.executionAsyncId())
+  process.nextTick(function () {
+    console.log('after nextTick', hooks.executionAsyncId())
+    console.log(ctx);
+    cb.apply(null);
+  });
+}
+
 app.get('/test', (request, response) => {
   if (request.query.arg) {
     response.status(400).json({ status: 'oops!' });
   } else {
-    response.json({ status: 'ok' });
+    // response.json({ status: 'ok' });
+    dostuff(function () {
+      response.send('ok');
+    })
   }
 });
 app.listen(port);
